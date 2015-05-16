756b38aaeb6b32e4a0d74ff98310082173b0b2ef
Larry D Gardner <ldgalaxy@comcast.net>
2015-05-16 10:55:26 -0600

    Finishing up Part two of the tutorial

--- index.php
+++ index.php
@@ -1,5 +1,23 @@
 <?php
 
+$offset = 0;
+
+if(isset($_POST['text']) && isset($_POST['searchfor']) && isset($_POST['replacewith'])){
+   $text = $_POST['text'];
+   $search = $_POST['searchfor'];
+   $replace = $_POST['replacewith'];
+
+   if(!empty($text) && !empty($search) && !empty($replace)){
+
+
+
+
+   }else{
+      echo 'Please fill in all fields.';
+   }
+}
+
+
 
 
 ?>
@@ -10,6 +28,7 @@
    <textarea name="text" rows="6" cols="30"></textarea><br><br>
    Search for:<br>
    <input type="text" name="searchfor"><br><br>
+   Replace with:<br>
    <input type="text" name="replacewith"><br><br>
    <input type="submit" value="Find and Replace">
 
