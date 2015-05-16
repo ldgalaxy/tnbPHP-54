28559b9220f37f195f78d77ae301a085936ef13a
Larry D Gardner <ldgalaxy@comcast.net>
2015-05-16 08:46:11 -0600

    Building the input part of the web page

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+
+
+?>
+
+
+<form action="index.php" method="POST">
+
+   <textarea name="text" rows="6" cols="30"></textarea><br><br>
+   Search for:<br>
+   <input type="text" name="searchfor"><br><br>
+   <input type="text" name="replacewith"><br><br>
+   <input type="submit" value="Find and Replace">
+
+</form>
