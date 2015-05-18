55ff4ae7af65bd6194a6e6c10ce7422a0ef2faf9
Larry D Gardner <ldgalaxy@comcast.net>
2015-05-18 12:47:40 -0600

    end of Part 3 of Find a d Replace app

--- index.php
+++ index.php
@@ -6,30 +6,29 @@ if(isset($_POST['text']) && isset($_POST['searchfor']) && isset($_POST['replacew
    $text = $_POST['text'];
    $search = $_POST['searchfor'];
    $replace = $_POST['replacewith'];
+   $search_length = strlen($search);
 
    if(!empty($text) && !empty($search) && !empty($replace)){
+      while($strpos = strpos($text, $search, $offset)){
+      echo $strpos . '<br>';
+      echo $offset = $strpos + $search_length . '<br>';         
+      
+      }      
 
 
-
-
+       
    }else{
       echo 'Please fill in all fields.';
    }
 }
 
-
-
-
 ?>
 
-
-<form action="index.php" method="POST">
-
+<form action="index.php" method="POST"">
    <textarea name="text" rows="6" cols="30"></textarea><br><br>
    Search for:<br>
    <input type="text" name="searchfor"><br><br>
    Replace with:<br>
    <input type="text" name="replacewith"><br><br>
    <input type="submit" value="Find and Replace">
-
 </form>
